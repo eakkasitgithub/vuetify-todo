3d8b1f061b232e7bb4278d3d178087f9088b2da9
Emigdio Torres Ruiz <emigdio821@gmail.com>
2019-01-26 20:39:14 -0600

    added fontawesome icons, added WIP footer

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,20 @@
     <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
-  components: { Navbar },
+  components: { Navbar, Footer },
   data () {
-    return {
-      //
-    }
+    return {}
   }
 }
 </script>
